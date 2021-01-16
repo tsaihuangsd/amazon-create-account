365d6a3d590ad4cc0e1d4f43b58a74baf3556436
Tsai Huang <tsaihuangsd@gmail.com>
2021-01-15 17:09:10 -0800

    add amazon logo at top

---
+++ src/MVP/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="a-icon-logo" />
+        
+        
+    </div>
+  );
+}
+
+export default App;

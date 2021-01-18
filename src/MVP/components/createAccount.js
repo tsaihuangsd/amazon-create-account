9e79ed90f5979e269314e58ce6ec6431ec0ae552
Tsai Huang <tsaihuangsd@gmail.com>
2021-01-18 01:05:28 -0800

    Add additional styling to create-account-box.  Implement hooks and handle change for input fields

--- src/MVP/components/createAccount.js
+++ src/MVP/components/createAccount.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import CreateAccountView from '../views/createAccountView';
 
 const CreateAccount = () =>{
-    
+    const [state, setState] = useState({
+        name:"", email:"", password:"", rePassword:""
+    });
+
+    const handleChange=(e)=>{
+        setState({[e.target.id]: e.target.value});
+        console.log(e.target.id, ": ", e.target.value)
+    }
 
     return (
-        <CreateAccountView />
+        <CreateAccountView 
+            name={state.name} 
+            email={state.email} 
+            password={state.password} 
+            rePassword={state.rePassword} 
+            handleChange={handleChange}/>
     );
 }
 
--- src/MVP/views/createAccountView.js
+++ src/MVP/views/createAccountView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styling/App.css';
 import * as Constants from '../constants';
 
-const CreateAccountView = () =>{    
+const CreateAccountView = ({name, email, password, rePassword, handleChange}) =>{    
     return (
         <div className="main-container">
             <div className="a-icon-logo" />
@@ -10,16 +10,16 @@ const CreateAccountView = () =>{
                 <div className="create-account-box">
                     <h2>Create account</h2>             
                     <form>
-                        <label for="name">Your name</label><br/>
-                        <input type="text" id="name"></input><br/>
+                        <label>Your name</label><br/>
+                        <input type="text" id="name" value={name} onChange={handleChange}></input><br/><br/>
                         <label>Email</label><br/>
-                        <input type="text" id="email"></input><br/>
+                        <input type="text" id="email" value={email} onChange={handleChange}></input><br/><br/>
                         <label>Password</label><br/>
-                        <input type="text" id="password"></input><br/>
-                        {/* <h5>Passwords must be at least 6 characters.</h5> */}
+                        <input type="text" id="password" placeholder="At least 6 characters" value={password} onChange={handleChange}></input>
+                        <h5 id="passord-requirement">Passwords must be at least 6 characters.</h5><br/>
                         <label>Re-enter password</label><br/>
-                        <input type="text" id="reenter-password"></input><br/>
-                        <input type="submit" value="Create your Amazon account"></input>
+                        <input type="text" id="reenter-password" value={rePassword} onChange={handleChange}></input><br/><br/>
+                        <input type="submit" id="create-account-button" value="Create your Amazon account"></input><br/><br/>
                     </form>
                     <h5>By creating an account, you agree to Amazon's <a href={Constants.LINK_CONDITION_OF_USE}>Conditions of Use</a> and <a href={Constants.LINK_PRIVACY_NOTICE}>Privacy Notice</a>.
                     </h5>
